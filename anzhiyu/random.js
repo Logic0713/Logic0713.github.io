85ce91a2b1ca4d79cf2583c2d5f6f0a4eed78c23
Logic0713 <3020044608@qq.com>
2024-09-13 01:12:10 +0800

    Site updated: 2024-09-13 01:12:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/07/hello-world/","2024/09/03/Docker初学/"];function toRandomPost(){
+var posts=["2024/07/07/hello-world/","2024/09/03/Docker初学/","2024/09/13/dvwa靶场练习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
